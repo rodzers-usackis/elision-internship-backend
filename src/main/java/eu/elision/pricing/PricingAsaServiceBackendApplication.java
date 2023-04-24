e99bbe8db1b9aed56028d5981cea7f72993597bb
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-24 15:13:00 +0200

    Added Checkstyle.

--- src/main/java/eu/elision/pricing/PricingAsaServiceBackendApplication.java
+++ src/main/java/eu/elision/pricing/PricingAsaServiceBackendApplication.java
@@ -3,11 +3,14 @@ package eu.elision.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
-public class PricingAsAServiceBackendApplication {
+public class PricingAsaServiceBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PricingAsAServiceBackendApplication.class, args);
+        SpringApplication.run(PricingAsaServiceBackendApplication.class, args);
     }
 
 }
