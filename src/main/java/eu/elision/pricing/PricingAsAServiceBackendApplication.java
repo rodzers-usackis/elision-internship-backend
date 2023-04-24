c726dd3a9ebdd64283548a89d67b4a030ed9ceb8
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-24 11:31:32 +0200

    Initial commit.

---
+++ src/main/java/eu/elision/pricing/PricingAsAServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package eu.elision.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingAsAServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingAsAServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/elision/pricing/PricingAsAServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.elision.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingAsAServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
