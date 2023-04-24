ec5c60303cd60be93699def92a817d2d10c457bb
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-24 11:47:00 +0200

    Added a test to check if the app runs without any error.

--- src/test/java/eu/elision/pricing/PricingAsAServiceBackendApplicationTests.java
+++ src/test/java/eu/elision/pricing/PricingAsAServiceBackendApplicationTests.java
@@ -1,13 +1,17 @@
 package eu.elision.pricing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PricingAsAServiceBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        assertDoesNotThrow(()-> SpringApplication.run(PricingAsAServiceBackendApplication.class));
     }
 
 }
