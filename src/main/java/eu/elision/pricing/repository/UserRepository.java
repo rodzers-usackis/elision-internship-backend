828dec4b54dce28c02bc8cd4293217278741024b
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-25 17:08:36 +0200

    Add User repository.

---
+++ src/main/java/eu/elision/pricing/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package eu.elision.pricing.repository;
+
+import eu.elision.pricing.domain.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+}
