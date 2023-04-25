2994c6aa02ef0e3fda565f0e8b410c1313a43ec3
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-25 17:19:34 +0200

    Format.

--- src/main/java/eu/elision/pricing/PricingAsaServiceBackendApplication.java
+++ src/main/java/eu/elision/pricing/PricingAsaServiceBackendApplication.java
@@ -3,9 +3,6 @@ package eu.elision.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- *
- */
 @SpringBootApplication
 public class PricingAsaServiceBackendApplication {
 
