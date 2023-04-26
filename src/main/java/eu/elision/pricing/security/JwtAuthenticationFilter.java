6af62e9907bfece8d9feccc43fafe3a5a274c6e1
Dominik Mika <Dominik.Mika@elision.eu>
2023-04-26 14:05:16 +0200

    minor corrections

--- src/main/java/eu/elision/pricing/security/JwtAuthenticationFilter.java
+++ src/main/java/eu/elision/pricing/security/JwtAuthenticationFilter.java
@@ -21,7 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
--- src/main/java/eu/elision/pricing/security/SecurityConfig.java
+++ src/main/java/eu/elision/pricing/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package eu.elision.pricing.security;
 
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
