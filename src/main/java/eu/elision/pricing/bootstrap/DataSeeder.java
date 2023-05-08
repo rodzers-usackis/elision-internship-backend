8e168b9cc1e34ff4cb8f83ad002405a6c5560a4c
Dominik Mika <Dominik.Mika@elision.eu>
2023-05-08 11:21:11 +0200

    Create hypertable Price in TimescaleDB

--- src/main/java/eu/elision/pricing/bootstrap/DataSeeder.java
+++ src/main/java/eu/elision/pricing/bootstrap/DataSeeder.java
@@ -3,19 +3,11 @@ package eu.elision.pricing.bootstrap;
 import eu.elision.pricing.domain.Role;
 import eu.elision.pricing.domain.User;
 import eu.elision.pricing.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 /**
  * This class is used to seed the database with some initial data.
@@ -27,11 +19,6 @@ public class DataSeeder implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
-
 
     @Override
     public void run(String... args) throws Exception {
