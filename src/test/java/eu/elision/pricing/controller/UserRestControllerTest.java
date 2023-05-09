1df0318503de447bf2f8c99fc0c1a01217c5bd7c
Dominik Mika <Dominik.Mika@elision.eu>
2023-05-09 16:00:28 +0200

    Correct tests

--- src/test/java/eu/elision/pricing/controller/UserRestControllerTest.java
+++ src/test/java/eu/elision/pricing/controller/UserRestControllerTest.java
@@ -19,11 +19,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 @SpringBootTest
 class UserRestControllerTest {
 
@@ -52,6 +54,8 @@ class UserRestControllerTest {
             .email("test@test.be")
             .role(Role.CLIENT)
             .build();
+
+        userRepository.save(user);
 
     }
 
