191d72590f83a48851886f74977feb35f743d90c
rodzers.usackis <rodzers.usackis@student.kdg.be>
2023-05-24 21:26:16 +0200

    Weird merge

--- src/test/java/eu/elision/pricing/service/AlertServiceImplTests.java
+++ src/test/java/eu/elision/pricing/service/AlertServiceImplTests.java
@@ -261,7 +261,7 @@ class AlertServiceImplTests {
         Product product = Product.builder()
             .name("Test product")
             .id(UUID.randomUUID())
-            .category(ProductCategory.ELECTRONICS)
+            .category(ProductCategory.CONSUMER_ELECTRONICS)
             .manufacturerCode("123A")
             .ean("1234567890123")
             .description("Test description")
