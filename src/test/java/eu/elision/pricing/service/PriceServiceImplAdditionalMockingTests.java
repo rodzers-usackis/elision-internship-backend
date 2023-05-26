0ca2516cbe4af314bb07844f895ce6e62e4b251c
Dominik Mika <Dominik.Mika@elision.eu>
2023-05-26 10:52:57 +0200

    Failing test still not fixed

--- src/test/java/eu/elision/pricing/service/PriceServiceImplAdditionalMockingTests.java
+++ src/test/java/eu/elision/pricing/service/PriceServiceImplAdditionalMockingTests.java
@@ -2,7 +2,6 @@ package eu.elision.pricing.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -42,16 +41,22 @@ class PriceServiceImplAdditionalMockingTests {
     private HttpRequestService httpRequestService;
 
     @MockBean
+    private ScraperService scraperService;
+
+    @MockBean
     private PriceRepository priceRepository;
 
     @Autowired
-    private PriceServiceImpl priceService;
+    private PriceService priceService;
 
     @Captor
     private ArgumentCaptor<Product> productCaptor;
 
     @Captor
     private ArgumentCaptor<List<Price>> priceListCaptor;
+
+    @Captor
+    private ArgumentCaptor<PriceScrapingConfig> priceScrapingConfigCaptor;
 
 
     @Test
@@ -60,54 +65,57 @@ class PriceServiceImplAdditionalMockingTests {
 
         Product p1 = Product.builder()
             .name("iPhone 14 Pro Max")
-            .id(UUID.randomUUID())
-            .category(ProductCategory.ELECTRONICS)
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1a"))
+            .category(ProductCategory.CONSUMER_ELECTRONICS)
             .build();
 
         Product p2 = Product.builder()
             .name("iPhone 10")
-            .id(UUID.randomUUID())
-            .category(ProductCategory.ELECTRONICS)
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1b"))
+            .category(ProductCategory.CONSUMER_ELECTRONICS)
             .build();
 
         RetailerCompany rc1 = RetailerCompany.builder()
             .name("Alternate.be")
-            .id(UUID.randomUUID())
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1c"))
             .build();
 
         RetailerCompany rc2 = RetailerCompany.builder()
             .name("Coolblue.be")
-            .id(UUID.randomUUID())
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1d"))
             .build();
 
         PriceScrapingConfig psc1 = PriceScrapingConfig.builder()
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1e"))
             .commaSeparatedDecimal(true)
             .cssSelector("span.price")
             .product(p1)
             .retailerCompany(rc1)
             .active(true)
             .url(
-                "invalid url here because the request should be mocked anyway https://www.alternate.be/Apple/iPhone-14-Pro-Max-smartphone/html/product/1866555")
+                "invalid url here because the request should be mocked anyway p1 psc1")
             .build();
 
         PriceScrapingConfig psc2 = PriceScrapingConfig.builder()
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c1f"))
             .commaSeparatedDecimal(true)
             .cssSelector("div.product-details span.price")
             .product(p1)
             .retailerCompany(rc2)
             .active(true)
             .url(
-                "invalid url here because the request should be mocked anyway coolblue.be")
+                "invalid url here because the request should be mocked anyway p1 psc2")
             .build();
 
         PriceScrapingConfig psc3 = PriceScrapingConfig.builder()
+            .id(UUID.fromString("f9153624-7234-46b7-b99c-28c255281638"))
             .commaSeparatedDecimal(true)
             .cssSelector("div.product-details span.price")
             .product(p2)
             .retailerCompany(rc2)
             .active(true)
             .url(
-                "another invalid url here because the request should be mocked anyway coolblue.be")
+                "another invalid url here because the request should be mocked anyway p2 psc3")
             .build();
 
 
@@ -128,6 +136,33 @@ class PriceServiceImplAdditionalMockingTests {
         when(httpRequestService.getHtml(psc3.getUrl())).thenReturn(
             "<div class=\"product-details\"><span class=\"price\">1000,99 €</span></div>");
 
+        Price pr1 = Price.builder()
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c20"))
+            .retailerCompany(rc1)
+            .product(p1)
+            .amount(1739.00)
+            .build();
+
+        Price pr2 = Price.builder()
+            .id(UUID.fromString("a0e8c1a0-1b1a-4b1a-8b1a-9b1a0b1a0c21"))
+            .retailerCompany(rc2)
+            .product(p1)
+            .amount(2100.99)
+            .build();
+
+        Price pr3 = Price.builder()
+            .id(UUID.fromString("f9153624-7234-46b7-b99c-28c255281639"))
+            .retailerCompany(rc2)
+            .product(p2)
+            .amount(1000.99)
+            .build();
+
+
+        when(scraperService.scrapePrice(psc1)).thenReturn(pr1);
+        when(scraperService.scrapePrice(psc2)).thenReturn(pr2);
+        when(scraperService.scrapePrice(psc3)).thenReturn(pr3);
+
+
         //when priceRepository.save(any) then return the argument that was originally passed
         when(priceRepository.save(any())).thenAnswer(
             (InvocationOnMock invocation) -> invocation.getArgument(0));
@@ -135,12 +170,18 @@ class PriceServiceImplAdditionalMockingTests {
 
         priceService.scrapeProductsPrices(productIds);
 
+
+        /*verify(httpRequestService).getHtml(psc2.getUrl());
         verify(httpRequestService).getHtml(psc1.getUrl());
-        verify(httpRequestService).getHtml(psc2.getUrl());
-        verify(httpRequestService).getHtml(psc3.getUrl());
+        verify(httpRequestService).getHtml(psc3.getUrl());*/
+
+        verify(scraperService, times(3)).scrapePrice(priceScrapingConfigCaptor.capture());
+
+        List<PriceScrapingConfig> capturedPscs = priceScrapingConfigCaptor.getAllValues();
+        assertThat(capturedPscs, containsInAnyOrder(psc1, psc2, psc3));
+
         verify(productPriceScrapedEventPublisher, times(2)).publish(productCaptor.capture(),
             priceListCaptor.capture());
-
 
 
         List<Product> capturedProducts = productCaptor.getAllValues();
