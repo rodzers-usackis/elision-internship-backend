034a89aef6f898a8dc9a6021f07c6314ceedba16
Dominik Mika <Dominik.Mika@elision.eu>
2023-05-30 11:37:01 +0200

    Handle 404 in controller

---
+++ src/main/java/eu/elision/pricing/exceptions/NotFoundExceptionHandler.java
@@ -0,0 +1,16 @@
+package eu.elision.pricing.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class NotFoundExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
